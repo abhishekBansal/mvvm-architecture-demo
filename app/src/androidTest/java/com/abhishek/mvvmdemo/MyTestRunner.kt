2c84fb2d5a589fd9cf78316ad422ab855ca9c054
Abhishek Bansal <doscover.ab@gmail.com>
2019-11-05 23:38:47 +0530

    + preparing for ui tests

---
+++ app/src/androidTest/java/com/abhishek/mvvmdemo/MyTestRunner.kt
@@ -0,0 +1,11 @@
+package com.abhishek.mvvmdemo
+
+import android.app.Application
+import android.content.Context
+import androidx.test.runner.AndroidJUnitRunner
+
+class MyTestRunner : AndroidJUnitRunner() {
+    override fun newApplication(cl: ClassLoader?, className: String?, context: Context?): Application {
+        return super.newApplication(cl, TestApplication::class.java.name, context)
+    }
+}
